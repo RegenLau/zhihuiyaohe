f5c5a2e32e58b2e199da66bd16b3393cac32f379
RegenLau <regenlau@RegenLaudeMacBook-Air.local>
2026-05-27 00:31:20 +0800

    fix: resolve frontend component import

--- saiadmin-base/frontend/src/main.js
+++ saiadmin-base/frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import ArcoVue from '@arco-design/web-vue'
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 
-import globalComponents from '@/components'
+import globalComponents from './components/index.js'
 import App from './App.vue'
 import router from './router'
 import store from './store'
